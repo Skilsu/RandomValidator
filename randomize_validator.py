d24e7b781719bb5e25ab4935b99f82792116f0e3
Nicolas <66683208+Skilsu@users.noreply.github.com>
2022-11-09 23:42:37 +0100

    Create randomize_validator.py
    Just a little ramdom analyzer.
    If you have a better or new idea, please feel free to share it.

---
+++ randomize_validator.py
@@ -0,0 +1,19 @@
+def random_variance(number_of_games=1000, repetition_of_game=10000):
+    erg = []
+    for a in range(int(number_of_games/2)):
+        erg.append(0)
+    for i in range(repetition_of_game):
+        var = 0
+        for j in range(number_of_games):
+            if bool(random.getrandbits(1)):
+                var += 1
+        var -= int(number_of_games / 2)
+        if var < 0:
+            var -= 2 * var
+        erg[var] = erg[var] + 1
+    var = 0
+    for idx, i in enumerate(erg):
+        var += i
+        if i > 0:
+            print(str(idx) + ": " + str(i) + " = " + str(idx / number_of_games * 100)[:4] + "% -> " +
+                  str(var / 10000 * 100)[:4] + "%")
